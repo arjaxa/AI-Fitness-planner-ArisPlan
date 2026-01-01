a651eecbb7f678f368eb7baf66b60402d8611369
arjaxa <ariana.kh21@gmail.com>
2026-01-01 14:15:58 +0330

    Update splits.py

--- splits.py
+++ splits.py
@@ -1,6 +1,125 @@
 custom_splits = {
     "3": {
-        "hyperthrophy":#...
+        "hyperthrophy": {
+            "beginner": [
+                # split 1
+                {
+                    "Day 1": [
+            ["BB Bench press | 10x3"],
+            ["Incline db bench press | 10x3"],
+            ["DB Fly | 12x3"],
+            ["Seated shoulder press | 12x3"],
+            ["Upright db row | 10x3"],
+            ["DB Lateral raise | 8x3"],
+            ["BB Shrug | 12x3"],
+            ["DB Overhead tricep extensions | 8x3"],
+            ["Triceps pushdown | 8x3"]
+                    ],
+                    "Day 2": [
+            ["Leg extensions 12x4"],
+            ["Leg press | 10x3"],
+            ["DB lunge | 12x3"],
+            ["BB RDL | 10x3"],
+            ["Hamstring curl machine | 10x4"],
+            ["Abduction machine | 12x4"],
+            ["Cable kickback | 20x3"],
+            ["Standing calve machine | 30x3"]            
+                    ],
+                    "Day 3": [
+            ["Lat pull down | 12x3"],
+            ["Row machine | 12x3"],
+            ["Incline db row | 10x3"],
+            ["Bent over db rear delt fly | 12x3"],
+            ["BB Bicep curl | 10x3"],
+            ["DB Hammer curl | 12x3"],
+            ["DB wrist curls | 10x3"],
+            ["Crunch machine | 8x3"],
+            ["Hanging leg raise| 8x3"],
+            ["Mountain climbers 12x3"]            
+                    ]
+                }
+                # split 2
+                {
+                    "Day 1": [
+            ["DB Bench press | 10x3"],
+            ["Incline db bench press | 10x3"],
+            ["DB Fly | 12x3"],
+            ["Standing shoulder press | 12x3"],
+            ["Upright bb row | 10x3"],
+            ["DB Lateral raise | 8x3"],
+            ["BB Shrug | 12x3"],
+            ["DB Overhead tricep extensions | 8x3"],
+            ["Triceps pushdown | 8x3"]            
+                    ],
+                    "Day 2": [
+            ["Leg press 12x4"],
+            ["Smith squats | 10x3"],
+            ["DB lunge | 12x3"],
+            ["BB RDL | 10x3"],
+            ["Hamstring curl machine | 10x4"],
+            ["Abduction machine | 12x4"],
+            ["Cable kickback | 20x3"],
+            ["Standing calve machine | 30x3"]            
+                    ],
+                    "Day 3": [
+            ["BB row | 12x3"],
+            ["H machine | 12x3"],
+            ["Incline db row | 10x3"],
+            ["Bent over db rear delt fly | 12x3"],
+            ["BB Bicep curl | 10x3"],
+            ["DB Hammer curl | 12x3"],
+            ["DB wrist curls | 10x3"],
+            ["Crunch machine | 8x3"],
+            ["Hanging leg raise| 8x3"],
+            ["Mountain climbers 12x3"]            
+                    ]
+                }
+            ],
+            "advanced": [
+                # split 1
+                {
+                    "Day 1": [...],
+                    "Day 2": [...],
+                    "Day 3": [...]
+                }
+                # split 2
+                {
+                    "Day 1": [...],
+                    "Day 2": [...],
+                    "Day 3": [...]
+                }
+            ]
+        }
+        "strenght": {
+            "beginner": [
+                # split 1
+                {
+                    "Day 1": [],
+                    "Day 2": [],
+                    "Day 3": []
+                }
+                # split 2
+                {
+                    "Day 1": [],
+                    "Day 2": [],
+                    "Day 3": []
+                }
+            ],
+            "advanced": [
+                # split 1
+                {
+                    "Day 1": [],
+                    "Day 2": [],
+                    "Day 3": []
+                }
+                # split 2
+                {
+                    "Day 1": [],
+                    "Day 2": [],
+                    "Day 3": []
+                }
+            ]
+        }
     },
 
     "4": {"4 Day Upper Lower (pull/leg(q)/push/leg(h) Custom1)": {
