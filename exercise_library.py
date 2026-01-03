3de442336e1e3635be9c5a2378830b45bedf8e8b
arjaxa <ariana.kh21@gmail.com>
2026-01-03 16:37:26 +0330

    Create exercise_library.py

---
+++ exercise_library.py
@@ -0,0 +1,58 @@
+EXERCISE_LIBRARY = [
+    # QUADS
+    {
+        "name": "Leg Extension",
+        "muscle": "quads",
+        "type": "isolation",
+        "equipment": "machine"
+    },
+     {
+        "name": "Back Squat",
+        "muscle": "quads",
+        "type": "compound",
+        "equipment": "barbell"
+    },
+     {
+        "name": "Goblet Squat",
+        "muscle": "quads",
+        "type": "compound",
+        "equipment": "dumbbell"
+    },
+    # CHEST
+     {
+        "name": "BB Bench Press",
+        "muscle": "chest",
+        "type": "compound",
+        "equipment": "barbell"
+    },
+     {
+        "name": "DB Bench Press",
+        "muscle": "chest",
+        "type": "compound",
+        "equipment": "dumbbell"
+    },
+    {
+        "name": "Cable fly",
+        "muscle": "chest",
+        "type": "isolation",
+        "equipment": "cable"
+    },
+    {
+        "name": "Lat Pulldown",
+        "muscle": "back",
+        "type": "compound",
+        "equipment": "machine"
+    },
+    {
+        "name": "Seated Cable Row",
+        "muscle": "back",
+        "type": "compound",
+        "equipment": "cable"
+    },
+    {
+        "name": "Pull-up",
+        "muscle": "back",
+        "type": "compound",
+        "equipment": "bodyweight"
+    }
+]
